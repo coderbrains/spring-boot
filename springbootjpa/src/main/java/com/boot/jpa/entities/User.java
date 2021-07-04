302d8723f9946983250741f3579714a1ce73bed5
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-04 18:25:25 +0530

    a new jpa concept is added

--- springbootjpa/src/main/java/com/boot/jpa/entities/User.java
+++ springbootjpa/src/main/java/com/boot/jpa/entities/User.java
@@ -1,7 +1,23 @@
 package com.boot.jpa.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter 
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
+	@Id
+	private int id;
+	private String name;
+	private String about;
+	private String address;
+	
 }
