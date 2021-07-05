91310a7ecc8af2edeba1c574e4b814384b9597e4
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-05 20:02:30 +0530

    all the commits of the initial is pushed

---
+++ jpa_controller_test/src/main/java/com/jpa/jpa_controller_test/JpaControllerTestApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.jpa_controller_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaControllerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaControllerTestApplication.class, args);
+	}
+
+}
---
+++ jpa_controller_test/src/test/java/com/jpa/jpa_controller_test/JpaControllerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.jpa_controller_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaControllerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
