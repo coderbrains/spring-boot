517430fc07ea591c6687731ed898129df6983c30
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-07 11:29:46 +0530

    initial commit of the project

---
+++ embededobjects/src/main/java/com/crud/emobeded/EmbededobjectsApplication.java
@@ -0,0 +1,13 @@
+package com.crud.emobeded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbededobjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbededobjectsApplication.class, args);
+	}
+
+}
---
+++ embededobjects/src/test/java/com/crud/emobeded/EmbededobjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.emobeded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbededobjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
