86c485bf165ea36d346b838dcd1cab38395d8ccc
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-09 23:33:13 +0530

    all the commits have been done for the uploading the file.

---
+++ embededobjects/src/main/java/com/crud/embeded/controller/FileController.java
@@ -0,0 +1,38 @@
+package com.crud.embeded.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.crud.embeded.helper.FileUploading;
+
+@RestController
+public class FileController {
+
+	@Autowired
+	private FileUploading fileUploading;
+	
+	@PostMapping("/upload-file")
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file){
+//		System.out.println(file);
+//		System.out.println(file.getOriginalFilename());
+//		System.out.println(file.getSize());
+//		System.out.println(file.getContentType());
+//		return ResponseEntity.ok("working...api");
+		
+		if(file.getContentType().equals("image/jpeg")) {
+
+			
+			boolean b = fileUploading.uploadFile(file);
+			return ResponseEntity.status(HttpStatus.OK).body("file uploaded successfully..");
+				
+		}else {
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body("File not uploaded. Only jpeg is accepted..");
+		}
+	}
+	
+}
---
+++ embededobjects/src/main/java/com/crud/embeded/helper/FileUploading.java
@@ -0,0 +1,53 @@
+package com.crud.embeded.helper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploading {
+
+	public final String UPLOAD_FILE = "D:\\springboot\\embededobjects\\src\\main\\resources\\static\\images";
+	
+	public boolean uploadFile(MultipartFile file) {
+		
+		
+		boolean f = false;
+		
+		try {
+			
+//			InputStream is = file.getInputStream();
+//			byte[] b = new byte[is.available()];
+//			is.read(b);
+//			
+//			FileOutputStream fos = new FileOutputStream(UPLOAD_FILE + File.separator + file.getOriginalFilename());
+//			fos.write(b);
+//			fos.close();
+//			fos.flush();
+			
+			Files.copy(file.getInputStream(), Paths.get(UPLOAD_FILE + File.separator +  
+					file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+			
+			f= true;
+			
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		
+		return f;
+		
+	}
+	
+	
+	
+}
