224720898e1e3a23f29fda8e32bc236e63525853
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-10 19:28:51 +0530

    all the checking of the validation like the email and the username is done with the valid annotations

--- springvalidation/src/main/java/com/spring/entity/LoginCredentials.java
+++ springvalidation/src/main/java/com/spring/entity/LoginCredentials.java
@@ -1,5 +1,6 @@
 package com.spring.entity;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -24,5 +25,8 @@ public class LoginCredentials {
 	
 	@Pattern(regexp ="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$" , message= "Please enter the valid email")
 	private String email;
+	
+	@AssertTrue(message = "Please check the terms and conditions.")
+	private boolean agreed;
 
 }
