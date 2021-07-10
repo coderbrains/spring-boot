4d556867c411c32bce302e98de70edb4a03151a8
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-10 13:45:28 +0530

    iteration in thymleaf

--- thymeleafproject/src/main/java/com/crud/thymeleaf/controller/Mycontroller.java
+++ thymeleafproject/src/main/java/com/crud/thymeleaf/controller/Mycontroller.java
@@ -1,9 +1,11 @@
 package com.crud.thymeleaf.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +20,15 @@ public class Mycontroller {
 		model.addAttribute("date", new Date());
 		
 		return "home";
+	}
+	
+	@GetMapping("/contact")
+	public String contact(Model model) {
+		
+		List<String> list = List.of("awaish", "mainsh", "nhgh");
+		
+		model.addAttribute("names", list);
+		return "contact";
 	}
 	
 }
