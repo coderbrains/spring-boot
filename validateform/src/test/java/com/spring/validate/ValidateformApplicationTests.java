e49d663d54b5648fb6fddd02145bc4d4ed1a9b90
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-11 11:51:51 +0530

    all the initial commit of the new project is added

---
+++ validateform/src/test/java/com/spring/validate/ValidateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.validate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
